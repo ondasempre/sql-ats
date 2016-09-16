39865d2a484216152db2417064dd9ef8d628c742
Flavio Macciocchi <ondasempre@gmail.com>
2016-09-16 17:16:09 +0200

    Comment

--- SQL_ATS/procedura.sql
+++ SQL_ATS/procedura.sql
@@ -1,7 +1,10 @@
--- Dichiaro delle variabili e creo un cursore in maniera esplicita
+-- DICHIARAZIONE DI UN CURSORE ESPLICITO PL/SQL
+
+-- dichiaro delle variabili segliendo il tipo del dato
 declare 
 c nominativi.cognome%TYPE;
 n nominativi.nome%TYPE;
+-- dichiarazione del cursore per ciclare sulla tab Nominativi (crata nel db Oracle 10g)
 cursor nominativi_cur is 
 select * from nominativi
 for update;
